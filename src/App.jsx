5304e1c954279409ef4fc8dc72c6426289ce79eb
rm86626 <106156564+rm86626@users.noreply.github.com>
2022-11-14 11:39:50 +0800

    react开发环境

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import Reg from "@/components/Reg";
+
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h3>App组件</h3>
+                <Reg/>
+            </div>
+        );
+    }
+}
+
+export default App;
