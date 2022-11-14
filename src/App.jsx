8bfcd23f5f9ecf13e03bd70cf21324ed38195f7f
rm86626 <106156564+rm86626@users.noreply.github.com>
2022-11-14 11:50:39 +0800

    deltodo

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ class App extends Component {
             <div>
                 <h3>App组件</h3>
                 <p>addTodo功能</p>
+                <p>deltodo功能</p>
             </div>
         );
     }
