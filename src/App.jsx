2b85a84493392ccad894d36dfb958056fae58706
rm86626 <106156564+rm86626@users.noreply.github.com>
2022-11-14 11:50:09 +0800

    addTodo

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import Reg from "@/components/Reg";
+
 
 
 class App extends Component {
@@ -7,7 +7,7 @@ class App extends Component {
         return (
             <div>
                 <h3>App组件</h3>
-                <Reg/>
+                <p>addTodo功能</p>
             </div>
         );
     }
